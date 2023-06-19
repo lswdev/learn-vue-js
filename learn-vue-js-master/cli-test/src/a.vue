66fb4ed0e5cca567dc6d07328f43db4331cdaa22
lswdev <lswsepia@gmail.com>
2023-06-19 14:04:23 +0900

    vue-cli 사용하여 cli-test 생

---
+++ learn-vue-js-master/cli-test/src/a.vue
@@ -0,0 +1,29 @@
+<!-- 
+  var appHeader = {
+  template : '<div>header</div>',
+  methods: {
+    addNum: function() {
+
+    }
+  }
+} 
+-->
+
+<template>
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    addNum : function(){
+      
+    }
+  }
+
+}
+</script>
+
+<style>
+/* css */
+</style>
