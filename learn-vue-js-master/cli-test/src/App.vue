33e9b38ccdd1a3609f9feca4ee086f7ac8f9aaa3
lswdev <lswsepia@gmail.com>
2023-06-19 16:18:35 +0900

    컴포넌트 호출 방법

--- learn-vue-js-master/cli-test/src/App.vue
+++ learn-vue-js-master/cli-test/src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 
+      1. <hello-world></hello-world>
+      2. <HelloWorld></HelloWorld>
+      3. <HelloWorld/>
+      모두 같은 컴포넌트를 호출한다.
+      하지만 visual studio에서 컴포넌트 따라가기 기능을 사용하기 위해서는 1번 양식을 따라야 한다.(vue 공식 홈페이지 명시)
+    -->
   </div>
 </template>
 
