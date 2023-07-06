615c1ace21a4152485f3ea8a4cd4151d35f03253
lswdev <lswsepia@gmail.com>
2023-07-06 13:50:09 +0900

    helloworld 삭제. app.vue 초기화

--- learn-vue-js-master/cli-test/src/App.vue
+++ learn-vue-js-master/cli-test/src/App.vue
@@ -1,37 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <!-- 
-      - 컴포넌트 명명법
-      1. <hello-world></hello-world>
-      2. <HelloWorld></HelloWorld>
-      3. <HelloWorld/>
-      모두 같은 컴포넌트를 호출한다.
-      하지만 visual studio에서 컴포넌트 따라가기 기능을 사용하기 위해서는 1번 양식을 따라야 한다.(vue 공식 홈페이지 명시)
-    -->
+  <div>
+    {{ str }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-
-export default { //인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: 'App',
-  components: {
-    HelloWorld
+export default {
+  data: function() {
+    return {
+      str: 'hi'
+    }
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="">
+  
 </style>
