e5eea756d45cff570cddd915f0f140f15bb7a412
lswdev <lswsepia@gmail.com>
2023-07-06 14:33:27 +0900

    event emit 구현

--- learn-vue-js-master/cli-test/src/App.vue
+++ learn-vue-js-master/cli-test/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <app-header :propsdata="str"></app-header>
+    <app-header :propsdata="str" @renew="renewStr"></app-header>
   </div>
 </template>
 
@@ -15,6 +15,12 @@ export default {
   },
   components: {
     'app-header': AppHeader
+  },
+  methods: {
+    // AppHeader 에서 this.$emit()으로 받은 @renew 이벤트에 fuction을 연결
+    renewStr: function() {
+      this.str = 'hi~~';
+    }
   }
 }
 </script>
--- learn-vue-js-master/cli-test/src/components/AppHeader.vue
+++ learn-vue-js-master/cli-test/src/components/AppHeader.vue
@@ -1,12 +1,19 @@
 <template lang="">
   <div>
-    <h1>Header</h1>
+    <h1>{{ propsdata }}</h1>
+
+    <button @click="sendEvent">send</button>
   </div>
 </template>
 
 <script>
 export default { 
-  props: ['propsdata']
+  props: ['propsdata'],
+  methods: {
+    sendEvent: function(){
+      this.$emit('renew');
+    }
+  }
 }
 </script>
 
