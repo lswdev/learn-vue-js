62b079e82becc7057b5df88d4027a982e4015da1
lswdev <lswsepia@gmail.com>
2023-07-06 16:53:11 +0900

    v-model , form  데이터 전송 구현

--- learn-vue-js-master/vue-form/src/App.vue
+++ learn-vue-js-master/vue-form/src/App.vue
@@ -1,26 +1,33 @@
-<template>
-  <form action="">
+<template @submit="submitForm">
+  <form>
     <div>
       <label for="">id : </label>
-      <input id="username" type="text">
+      <input id="username" type="text" v-model="username">
     </div>
     <div>
       <label for="password">pw : </label>
-      <input id="password" type="password">
+      <input id="password" type="password" v-model="password">
     </div>
-    <button>login</button>
+    <button type="submit" @click="submitForm">login</button>
   </form>
 </template>
+
 <script>
 export default {
-  
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function(event) {
+      console.dir(event)
+      event.preventDefault();
+      console.log(this.username, this.password)
+    }
+  }
 }
-
-
-
-
-
-
 
 /* 
   vue create vue-form
