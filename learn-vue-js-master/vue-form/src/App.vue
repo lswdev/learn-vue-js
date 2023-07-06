c793ff6169f7161f674acc4bc2d13ced71871d54
lswdev <lswsepia@gmail.com>
2023-07-06 16:15:07 +0900

    vue-form 프로젝트 생성 및 form base 추가

---
+++ learn-vue-js-master/vue-form/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <form action="">
+    <div>
+      <label for="">id : </label>
+      <input id="username" type="text">
+    </div>
+    <div>
+      <label for="password">pw : </label>
+      <input id="password" type="password">
+    </div>
+    <button>login</button>
+  </form>
+</template>
+<script>
+export default {
+  
+}
+
+
+
+
+
+
+
+/* 
+  vue create vue-form
+  cd vue-form
+  npm run serve(npm start)
+*/
+</script>
+<style lang="">
+  
+</style>
