5bc435752ea50fb32b3d5673c61cf40b239727af
lswdev <lswsepia@gmail.com>
2023-07-07 13:59:08 +0900

    axios 데이터 전송 및 form 구현2

--- learn-vue-js-master/vue-form/src/App.vue
+++ learn-vue-js-master/vue-form/src/App.vue
@@ -1,5 +1,5 @@
-<template @submit="submitForm">
-  <form>
+<template>
+  <form @submit.prevent="submitForm"><!-- event.preventDefault 동일 -->
     <div>
       <label for="">id : </label>
       <input id="username" type="text" v-model="username">
@@ -8,11 +8,13 @@
       <label for="password">pw : </label>
       <input id="password" type="password" v-model="password">
     </div>
-    <button type="submit" @click="submitForm">login</button>
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function() {
     return {
@@ -21,10 +23,25 @@ export default {
     }
   },
   methods: {
-    submitForm: function(event) {
-      console.dir(event)
-      event.preventDefault();
-      console.log(this.username, this.password)
+    submitForm: function() {
+      // event.preventDefault();   <---  vue에서는 v-on:submit.prevent를 사용
+      console.log(this.username, this.password);
+
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data ={
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+      .then(function(response){
+        console.log(response);
+      })
+      .catch( function(error){
+        console.log(error);
+      });      
+      // http 프로토콜 을 이용한  http통신 라이브러리
+      // 브라우저에서 서버로 데이터를 주고받기 위함
+
     }
   }
 }
@@ -33,6 +50,11 @@ export default {
   vue create vue-form
   cd vue-form
   npm run serve(npm start)
+*/
+
+/* 
+  npm i axios 
+  
 */
 </script>
 <style lang="">
